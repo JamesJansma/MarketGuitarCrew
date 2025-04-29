20d3660442dd8fd512d3c20b9ef2f10055da0dcd
JamesJansma <jjansma@zagmail.gonzaga.edu>
2025-04-29 10:01:32 -0700

    Updated server

--- server.py
+++ server.py
@@ -1,10 +1,14 @@
 # server.py
 from fastapi import FastAPI, HTTPException
+from fastapi.staticfiles import StaticFiles
 from pydantic import BaseModel
 from typing import List
 from src.guitarmarket.crew import Guitarmarket
 
 app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
 
 @app.get("/api/hello")
 def hello():
